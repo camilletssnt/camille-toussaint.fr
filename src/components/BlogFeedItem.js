0966b75b630facaf61cc144a70909ab07304d415
Stackbit Code Editor <projects@stackbit.com>
2021-09-07 17:46:48 +0000

    BlogFeedItem.js: changed

--- src/components/BlogFeedItem.js
+++ src/components/BlogFeedItem.js
@@ -13,7 +13,7 @@ export default class BlogFeedItem extends React.Component {
         const excerpt = _.get(post, 'excerpt');
         const date = _.get(post, 'date');
         const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
-        const formattedDate = d.toLocaleString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        const formattedDate = date.toLocaleString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
         const postUrl = getPageUrl(post, { withPrefix: true });
 
         return (
